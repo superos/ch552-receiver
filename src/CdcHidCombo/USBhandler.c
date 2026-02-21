0ea8d1fdad071de5264afb99d9692a9396799313
superos <superos@vip.qq.com>
2026-02-21 14:56:04 +0800

    prepare outputReport handler

--- src/CdcHidCombo/USBhandler.c
+++ src/CdcHidCombo/USBhandler.c
@@ -357,6 +357,9 @@ void USB_EP0_OUT() {
     keyboardLedStatus = Ep0Buffer[0];
     UEP0_T_LEN = 0;
     UEP0_CTRL ^= bUEP_R_TOG;
+
+    extern void hidOutputCallback(uint8_t);
+    hidOutputCallback(keyboardLedStatus);
   } else {
     UEP0_T_LEN = 0;
     UEP0_CTRL |= UEP_R_RES_ACK | UEP_T_RES_NAK; // Respond Nak
